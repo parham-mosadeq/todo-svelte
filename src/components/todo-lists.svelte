12ad172bbc6bbbc9dc320e3671be8b41be985c15
= <pmosadeqzadeh@gmail.com>
2025-12-09 22:15:41 +0330

    chore: added todo list

---
+++ src/components/todo-lists.svelte
@@ -0,0 +1,53 @@
+<script>
+  const todos=[
+
+  {
+    id:1,
+    title:"SOme random job",
+    desc:"SOMSFSDFDF:SJKDF:LSKDJF",
+    isComplete:false
+  },
+{
+    id:1,
+    title:"SOme random job",
+    desc:"SOMSFSDFDF:SJKDF:LSKDJF",
+    isComplete:true
+  },{
+    id:1,
+    title:"SOme random job",
+    desc:"SOMSFSDFDF:SJKDF:LSKDJF",
+    isComplete:false
+  },{
+    id:1,
+    title:"SOme random job",
+    desc:"SOMSFSDFDF:SJKDF:LSKDJF",
+    isComplete:true
+  },
+  ]
+</script>
+
+<div>
+  <h2>here is your lists</h2>
+  <ul>
+   {#each todos as todo }
+      <li class={`${todo.isComplete ? "done":"not-done"}`}>
+        <h3>
+          {todo.title}
+        </h3>
+        <p>
+          {todo.desc}
+        </p>
+        isComplete:{todo.isComplete}
+      </li>
+      {/each}
+    </ul>
+</div>
+
+<style>
+  .done{
+    text-decoration: line-through;
+  }
+  .not-done{
+    text-decoration: underline;
+  }
+</style>
